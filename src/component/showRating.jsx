77082dde081874acd10921d98642097b91d6d94d
AbdelghafourELouakkassi <125968638+AbdelghafourELouakkassi@users.noreply.github.com>
2023-08-11 16:24:10 +0100

    Update showRating.jsx

--- src/component/showRating.jsx
+++ src/component/showRating.jsx
@@ -1,8 +1,8 @@
 //this function used to show the rating that user select from the form in the review list
-function showRating({rate}){
+function showRating(rate){
         let stars=[]
         for(let i=1; i<=rate;i++){
-            stars.push(<i className="bi bi-star-fill text-warning" />)
+            stars.push(<i className="bi bi-star-fill text-warning" ></i>)
             
         }
         return(<div>{stars}</div>)
