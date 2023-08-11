bc6451bb0d6b7195bd26553160b50eb6f20c51f6
AbdelghafourELouakkassi <125968638+AbdelghafourELouakkassi@users.noreply.github.com>
2023-08-11 15:52:19 +0100

    Update showRating.jsx

--- src/component/showRating.jsx
+++ src/component/showRating.jsx
@@ -1,64 +1,17 @@
 //this function used to show the rating that user select from the form in the review list
 function showRating(rate){
 
-    if(rate===1){
+    if(rate){
             return(
                     <i className="bi bi-star-fill text-warning"></i>
                   )
     }
 
-    else if(rate===2){
-            return(
-                <>
-                    <i className="bi bi-star-fill text-warning"></i>
-                    <i className="bi bi-star-fill text-warning"></i>
-                </>
-            )
-      
-    }
-
-     else if(rate===3){
-            return(
-                <>
-                    <i className="bi bi-star-fill text-warning"></i>
-                    <i className="bi bi-star-fill text-warning"></i>
-                    <i className="bi bi-star-fill text-warning"></i>
-                </>
-      
-                )
-     }
-
-    else if (rate===4){
-            return(
-                <>
-                    <i className="bi bi-star-fill text-warning"></i>
-                    <i className="bi bi-star-fill text-warning"></i>
-                    <i className="bi bi-star-fill text-warning"></i>
-                    <i className="bi bi-star-fill text-warning"></i>
-                </>
-                  )
-
-    }             
-    
-    else if(rate===5){
-            return(
-                <>
-                    <i className="bi bi-star-fill text-warning"></i>
-                    <i className="bi bi-star-fill text-warning"></i>
-                    <i className="bi bi-star-fill text-warning"></i>
-                    <i className="bi bi-star-fill text-warning"></i>
-                    <i className="bi bi-star-fill text-warning"></i>
-                </>
-                  )
-    }
-
+ 
     else
          return 'something wrong'
            
-
     }
-
-  
 
 
   export default showRating;
